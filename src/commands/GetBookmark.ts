2fa1542d7b61addfc5a7ba17952ecbcf5ec48d06
Tom <t.sykes@pm.me>
2022-03-19 15:42:08 +0000

    Adding getbookmark command

---
+++ src/commands/GetBookmark.ts
@@ -0,0 +1,44 @@
+import { BaseCommandInteraction, Client, Constants, MessageActionRow, MessageSelectMenu } from 'discord.js';
+import { SlashCommand } from '../types/Command';
+import { openDbConnection } from '../utils/db';
+
+interface Bookmark {
+	bookmark_id: number;
+	user_id?: string;
+	bookmark_name: string;
+	data: string;
+}
+
+export const GetBookmark: SlashCommand = {
+	name: 'getbookmark',
+	description: 'Find a bookmark you saved',
+	type: 'CHAT_INPUT',
+	run: async(_: Client, interaction: BaseCommandInteraction) => {
+		const { user: { id: userId } } = interaction;
+		const db = openDbConnection();
+		const bookmarks: Bookmark[] = db
+			.prepare('SELECT bookmark_id, bookmark_name, data FROM bookmarks WHERE user_id = ?')
+			.all(userId);
+
+		await interaction.reply({
+			content: 'You\'ve saved the following bookmarks:',
+			components: [
+				new MessageActionRow()
+					.addComponents(
+						new MessageSelectMenu()
+							.setCustomId('GetBookmark')
+							.setPlaceholder('Pick a bookmark')
+							.addOptions(bookmarks
+								.map((bookmark) =>
+									({
+										label: bookmark.bookmark_name,
+										description: bookmark.data,
+										value: bookmark.data
+									})
+								)
+							)
+					)
+			]
+		})
+	}
+}
