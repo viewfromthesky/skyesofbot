144da099c16b0d5e62151fa2dc9cb2c72961c38f
Tom <t.sykes@pm.me>
2022-06-05 14:20:51 +0100

    refactoring getOperator/getOperatorName to work with any member

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,37 +1,48 @@
 import { Client, GuildMember } from 'discord.js';
 import Quote from '../types/Quote';
 
-export function getOperator(client: Client): GuildMember | undefined {
-  if (!process.env.GUILD_ID || !process.env.OPERATOR_ID) {
-    return undefined;
-  }
-
-  const { GUILD_ID: guildId, OPERATOR_ID: operatorId } = process.env;
+export function getMember(
+  client: Client,
+  guildId: string,
+  memberId: string
+): GuildMember | undefined {
   const guild = client.guilds.cache.find((guild) => guild.id === guildId);
 
   if (guild) {
-    const operator = guild?.members.cache.find(
-      (member) => member.id === operatorId
+    const member = guild?.members.cache.find(
+      (member) => member.id === memberId
     );
 
-    if (operator) {
-      return operator;
+    if (member) {
+      return member;
     }
   }
 
   return undefined;
 }
 
-export function getOperatorName(client: Client): string {
-  const operator = getOperator(client);
-
-  if (operator?.nickname) {
-    return operator.nickname;
-  } else if (operator?.user.username) {
-    return operator.user.username;
+export function getMemberName(member?: GuildMember): string | undefined {
+  if (member?.nickname) {
+    return member.nickname;
+  } else if (member?.user.username) {
+    return member.user.username;
   }
 
-  return 'an admin';
+  return undefined;
+}
+
+export function getOperator(client: Client): GuildMember | undefined {
+  if (!process.env.GUILD_ID || !process.env.OPERATOR_ID) {
+    return undefined;
+  }
+
+  const { GUILD_ID: guildId, OPERATOR_ID: operatorId } = process.env;
+
+  return getMember(client, guildId, operatorId);
+}
+
+export function getOperatorName(client: Client): string {
+  return getMemberName(getOperator(client)) || 'an admin';
 }
 
 export function buildQuote(quote: Quote): string {
