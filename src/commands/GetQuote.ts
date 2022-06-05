c7b2ed72a1391febbdbf36bf5243e7c15a8b73f0
Tom <t.sykes@pm.me>
2022-06-05 17:49:39 +0100

    Enabling random selection if ID not specified

--- src/commands/GetQuote.ts
+++ src/commands/GetQuote.ts
@@ -8,6 +8,7 @@ import {
 import { SlashCommand } from '../types/Command';
 import { openDbConnection } from '../utils/db';
 import Quote from '../types/Quote';
+import { getOperatorName } from '../utils/helpers';
 
 const GetQuote: SlashCommand = {
   name: 'getquote',
@@ -17,53 +18,61 @@ const GetQuote: SlashCommand = {
     {
       name: 'quote_id',
       description: "The numeric ID of the quote you'd like to print.",
-      required: true,
+      required: false,
       type: Constants.ApplicationCommandOptionTypes.INTEGER
     }
   ],
-  run: async (_: Client, interaction: BaseCommandInteraction) => {
+  run: async (client: Client, interaction: BaseCommandInteraction) => {
     const { options, user } = interaction;
     const quoteId = (options.get('quote_id')?.value as number) || 0;
+    let quote: Quote | undefined;
 
     if (quoteId) {
       const db = openDbConnection();
-      const quote: Quote = db
+      quote = db
         .prepare('SELECT * FROM quotes WHERE quote_id = ?')
         .get(quoteId);
 
-      if (quote) {
-        await interaction.reply({
-          ephemeral: true,
-          content: `You've selected quote #${quoteId}, "${quote.quote_name}"`,
-          components: [
-            new MessageActionRow().addComponents(
-              new MessageButton()
-                .setCustomId(`SendQuoteToChannel-${quoteId}`)
-                .setLabel('Send to channel')
-                .setStyle('PRIMARY'),
-              new MessageButton()
-                .setCustomId(`DeleteQuote-${quoteId}-${user.id}`)
-                .setLabel('Delete quote')
-                .setStyle('DANGER')
-            )
-          ]
-        });
-      } else {
-        // No quote was found with this ID
-        await interaction.reply({
-          ephemeral: true,
-          content: 'No quote exists with this ID. Please try another ID.'
-        });
-      }
-
       db.close();
     } else {
-      // The quote ID is invalid
+      const db = openDbConnection();
+      const allQuotes: Quote[] = db.prepare('SELECT * FROM quotes').all();
+
+      if (allQuotes.length) {
+        quote = allQuotes[Math.ceil(Math.random() * (allQuotes.length - 1))];
+      } else {
+        await interaction.reply({
+          ephemeral: true,
+          content:
+            'There are no quotes available to select from. Try adding a new one with `/savequote`'
+        });
+      }
+    }
+
+    if (quote) {
       await interaction.reply({
         ephemeral: true,
-        content:
-          'This ID is invalid. Quote IDs are numeric, please ensure you enter a number.'
+        content: `You've selected quote #${quoteId}, "${quote.quote_name}"`,
+        components: [
+          new MessageActionRow().addComponents(
+            new MessageButton()
+              .setCustomId(`SendQuoteToChannel-${quoteId}`)
+              .setLabel('Send to channel')
+              .setStyle('PRIMARY'),
+            new MessageButton()
+              .setCustomId(`DeleteQuote-${quoteId}-${user.id}`)
+              .setLabel('Delete quote')
+              .setStyle('DANGER')
+          )
+        ]
       });
+    } else {
+      await interaction.reply({
+        ephemeral: true,
+        content: `No quote was selected. If you think this is an error, contact ${getOperatorName(
+          client
+        )}.`
+      })
     }
   }
 };
