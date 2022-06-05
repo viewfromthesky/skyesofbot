31f1a8647ba0552dd96e31adf4ce3c9ae15ce3d8
Tom <t.sykes@pm.me>
2022-06-05 12:25:35 +0000

    Adding missing error message

--- src/commands/DeleteQuote.ts
+++ src/commands/DeleteQuote.ts
@@ -36,10 +36,19 @@ const DeleteQuote: SlashCommand = {
           )
           .run(quoteId, user.id);
 
+        console.log('update:', update);
+
         if (update.changes && update.lastInsertRowid) {
           await interaction.reply({
             ephemeral: true,
             content: `Your quote "${quote.quote_name}" (ID ${quoteId}) has been successfully removed.`
+          });
+        } else {
+          await interaction.reply({
+            ephemeral: true,
+            content: `There was an error deleting your quote. Contact ${getOperatorName(
+              client
+            )} as they may have broken something.`
           });
         }
       } else {
